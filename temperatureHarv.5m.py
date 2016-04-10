c0881484b378efedf843c1844ab610cd3b22b49b
Harvey Delaney <Harvey@Harveys-MacBook-Air.local>
2016-04-11 00:09:09 +1000

    First commit

---
+++ temperatureHarv.5m.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python
+
+import requests
+import json
+from num2words import num2words
+
+url = "api.openweathermap.org/data/2.5/weather?q=Canberra"
+
+response = requests.get("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22canberra%22)%20and%20u%3D'c'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+
+data= json.loads(response.text)
+
+temp=  data["query"]["results"]["channel"]["item"]["condition"]["temp"]
+
+condition = data["query"]["results"]["channel"]["item"]["condition"]["text"]
+
+temp = int(temp)
+
+
+if(temp<20 ):
+	colorStatus="blue"
+elif (temp>20 and temp < 30):
+	colorStatus = "orange"
+else:
+	colorStatus="red" 
+
+print num2words(temp)+  " degrees and " + condition.lower() + "| color = " + colorStatus
